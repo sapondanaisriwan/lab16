99d69818d5a7de46103c2c38947fe668bf88134f
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2025-02-01 18:47:05 +0700

    .

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,34 +1,85 @@
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
-#include<iomanip>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
 using namespace std;
 
-void showData(double *,int,int);
+void showData(double *, int, int);
 
-void randData(double *,int,int);
+void randData(double *, int, int);
 
-void findRowSum(const double *,double *,int,int);
+void findRowSum(const double *, double *, int, int);
 
-void findColSum(const double *,double *,int,int);
+// void findColSum(const double *, double *, int, int);
 
-int main(){
+int main()
+{
 	srand(time(0));
 	const int N = 6, M = 8;
 	double data[N][M] = {};
 	double sum1[N] = {};
 	double sum2[M] = {};
 	double *dPtr = data[0];
-	randData(dPtr,N,M);
-	showData(dPtr,N,M);
-	
+	randData(dPtr, N, M);
+	showData(dPtr, N, M);
+
+	cout << "\n---------------------------------------------\n";
+
+	findRowSum(dPtr, sum1, N, M);
+	showData(sum1, N, 1);
+
 	cout << "---------------------------------------------\n";
-	
-	findRowSum(dPtr,sum1,N,M);
-	showData(sum1,N,1);
-	
-	cout << "---------------------------------------------\n";
-	
-	findColSum(dPtr,sum2,N,M); 
-	showData(sum2,1,M);
+
+	findColSum(dPtr, sum2, N, M);
+	showData(sum2, 1, M);
+}
+
+void showData(double *arr, int N, int M)
+{
+	cout << fixed << setprecision(2);
+	for (int i = 0; i < N * M; i++)
+	{
+		cout << *(arr + i);
+		if ((i + 1) % M == 0)
+			cout << endl;
+		else
+			cout << " ";
+	}
+}
+
+void randData(double *arr, int N, int M)
+{
+	for (int i = 0; i < N * M; i++)
+	{
+		double randNum = ((rand() % 101)) / 100.00;
+		*(arr + i) = randNum;
+	}
+}
+
+void findRowSum(const double *arr, double *b, int N, int M)
+{
+	double sum = 0;
+	int round = 0;
+	for (int i = 0; i < N * M; i++)
+	{
+		sum = sum + *(arr + i);
+		if ((i + 1) % M == 0)
+		{
+			*(b + round) = sum;
+			sum = 0;
+			round++;
+		}
+	}
+}
+
+void findColSum(const double *a, double *b, int N, int M)
+{
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			b[j] += *(a + i * M + j);
+		}
+	}
 }
